8417e4d02efedcd1ccf0d39559945d4f14710a46
DESKTOP-B6BV7ML\Neo <steam.com77@yandex.ru>
2020-10-10 22:09:45 +0400

    Strategy

---
+++ OOP_Patterns/Strategy/Entityes/FasterRocket.cs
@@ -0,0 +1,18 @@
+﻿using Strategy.Abstract;
+using Strategy.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Entityes
+{
+    class FasterRocket : AbstractRocket, IMove
+    {
+        public void Moveble()
+        {
+            Console.WriteLine("MegaFaster");
+        }
+    }
+}
---
+++ OOP_Patterns/Strategy/Program.cs
@@ -0,0 +1,34 @@
+﻿using Strategy.Abstract;
+using Strategy.Entityes;
+using Strategy.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractRocket rocketOne = new SlowRocket { Type = "Heavy",Name = "Falcon9" };
+
+            AbstractRocket rocketSecodn = new FasterRocket { Type = "Not Heavy", Name = "Falcon1" };
+
+            Console.WriteLine(rocketOne.Name);
+
+
+            Console.WriteLine(rocketSecodn.Name);
+
+            var rocket = rocketOne as IMove;
+            var rocket1 = rocketSecodn as IMove;
+
+            rocket.Moveble();
+
+
+            rocket1.Moveble();
+        }
+    }
+}
